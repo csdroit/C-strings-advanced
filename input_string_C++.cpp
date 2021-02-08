df19f1e0377dc49737eb2f64478017e5f3d67dff
csdroit <73106581+csdroit@users.noreply.github.com>
2021-02-08 15:42:18 -0600

    Create input_string_C++.cpp

---
+++ input_string_C++.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string myString; //defining a variable string
+    getline(cin, myString);
+    cout << myString << endl;
+    return 0;
+}
