a2a041a0631c117647af96a82c835e22107f2ec8
csdroit <73106581+csdroit@users.noreply.github.com>
2021-02-09 12:10:10 -0600

    Create remove_deplicated_charachters_from_string.cpp

---
+++ remove_deplicated_charachters_from_string.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+# include<string>
+# include<cstring>        // For memset function
+using namespace std;
+
+
+int main() {
+    string input,output;        // defining two strings : input, output
+    bool alphabets[26];  // Assuming your string contains charactes between a-z only.
+    memset(alphabets,false,26);
+    for(int i=0;i<input.size();i++)
+    {
+        if(alphabets[input.at(i)-'a']==false)    // If that character was marked false i.e if it was not present in original 
+        {
+            alphabets[input.at(i)-'a']=true;    // mark that character true
+            output.push_back(input.at(i));           // Insert that character in string p
+        }
+    }
+    cout<<"Original String: "<<input<<endl; 
+    cout<<"String without duplicates: "<<output;
+    return 0;
+}
